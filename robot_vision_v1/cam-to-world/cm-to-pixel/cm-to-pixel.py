5ddc97e5b29884dbcf9a3f61bc5633d32eb87c78
codewithlennylen <codewithlennylen254@gmail.com>
2023-07-04 10:57:59 +0300

    Captured Images to Validate Coordinate Conversion.

--- robot_vision_v1/cam-to-world/cm-to-pixel/cm-to-pixel.py
+++ robot_vision_v1/cam-to-world/cm-to-pixel/cm-to-pixel.py
@@ -23,7 +23,7 @@ Y_AXIS_MIDPOINT_WORLD = X_AXIS_MIDPOINT_CAMERA
 Z_AXIS_MIDPOINT_WORLD = Y_AXIS_MIDPOINT_CAMERA
 
 
-coordinate_list = [
+coordinate_list_0 = [
     {
         "x1": 153.46231079101562,
         "y1": 482.2259521484375,
@@ -39,7 +39,111 @@ coordinate_list = [
 ]
 
 
+# validation-images
+coordinate_list_1 = [
+    {
+        "x1": 101.38385009765625,
+        "y1": 491.5885009765625,
+        "x2": 220.65567016601562,
+        "y2": 594.26318359375
+    },
+    {
+        "x1": 238.24143981933594,
+        "y1": 481.2621765136719,
+        "x2": 357.203125,
+        "y2": 589.399658203125
+    },
+    {
+        "x1": 386.10809326171875,
+        "y1": 472.27685546875,
+        "x2": 522.5375366210938,
+        "y2": 596.74267578125
+    }
+]
+
+coordinate_list_2 = [
+    {
+        "x1": 97.16504669189453,
+        "y1": 487.7280578613281,
+        "x2": 205.7490234375,
+        "y2": 591.25341796875
+    },
+    {
+        "x1": 250.60635375976562,
+        "y1": 344.41949462890625,
+        "x2": 376.4057312011719,
+        "y2": 442.16717529296875
+    },
+    {
+        "x1": 392.09490966796875,
+        "y1": 474.839599609375,
+        "x2": 539.689208984375,
+        "y2": 604.5635986328125
+    }
+
+]
+
+coordinate_list_3 = [
+    {
+        "x1": 97.1309585571289,
+        "y1": 488.7786865234375,
+        "x2": 207.25466918945312,
+        "y2": 590.373046875
+    },
+    {
+        "x1": 239.624755859375,
+        "y1": 61.00007629394531,
+        "x2": 378.1429443359375,
+        "y2": 153.8924560546875
+    },
+    {
+        "x1": 398.8424072265625,
+        "y1": 472.7543029785156,
+        "x2": 535.5258178710938,
+        "y2": 598.8170166015625
+    }
+
+]
+
+coordinate_list_4 = [
+    {
+        "x1": 221.8655242919922,
+        "y1": 524.8685913085938,
+        "x2": 361.63189697265625,
+        "y2": 639.1655883789062
+    },
+    {
+        "x1": 221.26693725585938,
+        "y1": 525.0765380859375,
+        "x2": 362.2241516113281,
+        "y2": 639.8843994140625
+    },
+    {
+        "x1": 390.501708984375,
+        "y1": 472.94232177734375,
+        "x2": 540.3988647460938,
+        "y2": 602.2704467773438
+    }
+]
+
+coordinate_list_5 = [
+    {
+        "x1": 54.059356689453125,
+        "y1": 490.96197509765625,
+        "x2": 196.0463409423828,
+        "y2": 623.2000122070312
+    },
+    {
+        "x1": 398.25164794921875,
+        "y1": 434.69219970703125,
+        "x2": 535.6445922851562,
+        "y2": 551.322021484375
+    }
+]
+
 # ---------- Camera Related Functions ----------
+
+
 def camera_inference():
     x, y, z = 10, 20, 30
 
@@ -149,7 +253,7 @@ print(f"Y_AXIS_MIDPOINT_CAMERA: {int(Y_AXIS_MIDPOINT_CAMERA)}")
 
 print("\n------\n")
 
-centroids = get_centroids(coordinate_list)
+centroids = get_centroids(coordinate_list_0)
 print(f"centroids: {centroids}")
 
 world_coordinates = get_world_coordinates(centroids)
@@ -163,6 +267,6 @@ print(f"3D_world_coordinates: {world_coordinates_3d}")
 
 
 print("\n")
-# validation_image = cv2.imread('validation-image.jpg')
-# cv2.imshow('validation_image', validation_image)
-# cv2.waitKey(0)
+validation_image = cv2.imread('./validation-images/validation-img-1.jpg')
+cv2.imshow('validation_image', validation_image)
+cv2.waitKey(0)
